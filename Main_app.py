dad31ac62ac9fcc7e8e4c6238f8aecb59cbab0ec
Abhinav Sharma <129160515+abhinav-Katiyan@users.noreply.github.com>
2024-09-29 11:19:07 +0530

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -32,26 +32,10 @@ display_button(kaggle_url, "Kaggle")
 
    
 
-# # Content for the right column
-# with col3:
-#     st.markdown(
-#         '<img src="https://media.licdn.com/dms/image/v2/D5603AQGxfctLmE8DQA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727202726874?e=1733356800&v=beta&t=fdzeagmU8UQNrmWtvTNu3zyKWDPuAmbp3nTr49nUkGk" class="profile-photo" width="360">',
-#         unsafe_allow_html=True
-#     )
-
-# Content for the left column
+# Content for the right column
 with col3:
     st.markdown(
-        """
-        <style>
-            .profile-photo {
-                border-radius: 50%; 
-                width: 360px;        
-                height: 360px;      
-            }
-        </style>
-        <img src="https://media.licdn.com/dms/image/v2/D5603AQGxfctLmE8DQA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727202726874?e=1733356800&v=beta&t=fdzeagmU8UQNrmWtvTNu3zyKWDPuAmbp3nTr49nUkGk" class="profile-photo">
-        """,
+        '<img src="https://media.licdn.com/dms/image/v2/D5603AQGxfctLmE8DQA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727202726874?e=1733356800&v=beta&t=fdzeagmU8UQNrmWtvTNu3zyKWDPuAmbp3nTr49nUkGk" class="profile-photo" width="360">',
         unsafe_allow_html=True
     )
 
