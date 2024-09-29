d12c5bf660e5f609a69355ff2c76e5b9070a4dd1
Abhinav Sharma <129160515+abhinav-Katiyan@users.noreply.github.com>
2024-09-29 11:12:01 +0530

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -21,12 +21,13 @@ col1, col2, col3 = st.columns([1.3, 0.2, 1])
 
 # Content for the left column
 with col1:
-    st.subherader("""
-    **Hello! I'm Abhinav Sharma** ) 
-    st.write("I'm a student passionate about data, aiming to become a skilled Data Analyst. I love transforming raw data into meaningful insights through data visualization, statistical analysis, and problem-solving.
-    
+    st.subheader("**Hello! I'm Abhinav Sharma**") 
+    st.write("""
+    I'm a student passionate about data, aiming to become a skilled Data Analyst. I love transforming raw data into meaningful insights through data visualization, statistical analysis, and problem-solving.
+
     Explore my portfolio to discover my projects and the tools and techniques I'm utilizing!
-    """")
+    """)
+
 
     # Display buttons for external links
     display_button(resume_url, "Resume")
